70f8926f66fe4091ced711a11a65562988a73a42
Marco A. Ramírez Hernández <marco.ramirez@utxicotepec.edu.mx>
2026-01-08 14:28:51 -0600

    Crear funciones JS y estilo del encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,27 @@
 function saludar(){
     alert("Bienvenido a la Academia de T.I");
 }
+function suma(){
+    var valor1 = 5;
+    var valor2 = 2;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma de" +valor1+" y "+valor2+" es: "+resultado);
+}
+function resta(){
+    var valor1 = 5;
+    var valor2 = 2;
+    var resultado = valor1 - valor2;
+    alert("Resultado de la resta de" +valor1+" y "+valor2+" es: "+resultado);
+}
+function mul(){
+    var valor1 = 5;
+    var valor2 = 2;
+    var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicacion de" +valor1+" y "+valor2+" es: "+resultado);
+}
+function div(){
+    var valor1 = 5;
+    var valor2 = 2;
+    var resultado = valor1 / valor2;
+    alert("Resultado de la division de" +valor1+" y "+valor2+" es: "+resultado);
+}
